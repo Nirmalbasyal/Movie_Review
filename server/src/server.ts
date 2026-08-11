35e99bbcb63bd6ada22efb990633090075ea8ef9
Nirmal Basyal <129817785+Nirmalbasyal@users.noreply.github.com>
2026-08-11 09:00:08 +0545

    update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
